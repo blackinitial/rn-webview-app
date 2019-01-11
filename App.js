d70dc0878518bde291055ceef0686487d99507c0
blackinitial <alhanif.blackinitial@gmail.com>
2019-01-11 14:03:29 +0700

    fix generate signed apk, add spinner when web load

--- App.js
+++ App.js
@@ -7,17 +7,40 @@
  */
 
 import React, {Component} from 'react';
-import {WebView} from 'react-native';
+import { View, WebView } from 'react-native';
+import Spinner from 'react-native-loading-spinner-overlay';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { visible: true };
+  }
+
+  showSpinner() {
+    this.setState({ visible: true });
+  }
+
+  hideSpinner() {
+    this.setState({ visible: false });
+  }
+
   render() {
-    let web = 'https://dev.airbinder.co.id/mobile/';
+    let web = 'https://www.instagram.com/';
 
     return (
-      <WebView
-        source={{uri: web}}
-        style={{marginTop: 20}}
-      />
+      <View style={{ flex: 1 }}>
+        <Spinner
+          visible={this.state.visible}
+          textContent={'Loading...'}
+          textStyle={{ color: '#fff' }}
+        />
+        <WebView
+          scalesPageToFit
+          source={{ uri: web }}
+          onLoadStart={() => (this.showSpinner())}
+          onLoad={() => (this.hideSpinner())}
+        />
+      </View>
     )
   }
 }
